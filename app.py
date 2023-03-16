f5769230ef21694b799aaa25f3a0ee2aaf7d3f50
maltamimi96 <46971446+maltamimi96@users.noreply.github.com>
2023-03-17 02:09:21 +1100

    adds users

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy 
 from flask_marshmallow import Marshmallow 
-import os
+import os 
 
 # Init app
 app = Flask(__name__)
@@ -36,6 +36,28 @@ class ProductSchema(ma.Schema):
 # Init schema
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
+
+
+
+
+#User Model
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(50), unique=True)
+  email = db.Column(db.String(100), unique=True)
+  password = db.Column(db.String(100))
+
+  def __init__(self, username, email, password):
+    self.username = username
+    self.email = email
+    self.password = password
+
+class UserSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'username', 'email', 'password')
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
 
 @app.before_first_request
 def create_tables():
@@ -95,6 +117,60 @@ def delete_product(id):
   db.session.commit()
 
   return product_schema.jsonify(product)
+
+
+#user Routes
+@app.route('/users', methods=['POST'])
+def add_user():
+  username = request.json['username']
+  email = request.json['email']
+  password = request.json['password']
+
+  new_user = User(username, email, password)
+
+  db.session.add(new_user)
+  db.session.commit()
+
+  return user_schema.jsonify(new_user)
+
+# Get All Users
+@app.route('/users', methods=['GET'])
+def get_users():
+  all_users = User.query.all()
+  result = users_schema.dump(all_users)
+  return jsonify(result)
+
+# Get Single User
+@app.route('/users/<id>', methods=['GET'])
+def get_user(id):
+  user = User.query.get(id)
+  return user_schema.jsonify(user)
+
+# Update a User
+@app.route('/users/<id>', methods=['PUT'])
+def update_user(id):
+  user = User.query.get(id)
+
+  username = request.json['username']
+  email = request.json['email']
+  password = request.json['password']
+
+  user.username = username
+  user.email = email
+  user.password = password
+
+  db.session.commit()
+
+  return user_schema.jsonify(user)
+
+# Delete User
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):  
+  user = User.query.get(id)
+  db.session.delete(user)
+  db.session.commit()
+
+  return user_schema.jsonify(user)
 
 # Run Server
 if __name__ == '__main__':
